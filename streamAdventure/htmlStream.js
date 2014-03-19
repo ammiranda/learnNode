6b53113ccee8d600b844f5d6285fe1062a73ce62
Alexander <alexandermichaelmiranda@gmail.com>
2014-03-19 16:09:54 -0700

    Added solution to htmlStream challenge

---
+++ streamAdventure/htmlStream.js
@@ -0,0 +1,12 @@
+var trumpet = require('trumpet');
+var fs = require('fs');
+var through = require('through');
+var tr = trumpet();
+
+var stream = tr.select('.loud').createStream();
+
+stream.pipe(through(function(buf){
+	this.queue(buf.toString().toUpperCase());
+	})).pipe(stream);
+
+process.stdin.pipe(tr).pipe(process.stdout);
