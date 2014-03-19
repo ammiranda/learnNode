d0f97a213c41e22076bc6bf18d1f82ca41493afa
Alexander <alexandermichaelmiranda@gmail.com>
2014-03-19 13:47:20 -0700

    Added concat.js file to stream-adventure solutions

--- streamAdventure/concat.js
+++ streamAdventure/concat.js
@@ -1,10 +1,6 @@
 var concat = require('concat-stream');
-var http = require('http');
 
-var server = http.createServer(function(req, res) {
-	req.pipe(concat(function(body) {
-		var reverseText = body.toString().split("").reverse().join("");
-		res.end(reverseText);
-		}));
-	}
-	server.listen(process.argv[2]);
+process.stdin.pipe(concat(function(body){
+	var reverseText = body.toString().split("").reverse().join("");
+	console.log(reverseText);
+	}));
